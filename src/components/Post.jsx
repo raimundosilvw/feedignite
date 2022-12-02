46516e9886ded65f9ec1808cbe3577cffdfab7a0
raimundosilvw <mengao-10r@hotmail.com>
2022-12-01 23:40:38 -0300

    Criação e estiliziação dos Components e do Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/raimundosilvw.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Raimundo Silva</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='1 de Dezembro às 21:49' dateTime="2022-12-01 21:49:30">Publicado há 1h</time>
+            </header>    
+
+            <div className={styles.content}>
+                <p> Fala galeraa 👋</p>
+                <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p> 👉 <a href="">jane.design/doctorcare</a></p>
+                <p> 
+                    <a href=""> #novoprojeto </a>
+                    <a href=""> #nlw </a>
+                    <a href=""> #rocketseat</a>
+
+                </p>
+            </div>                                                                                                                                   
+        </article>
+    )   
+}
