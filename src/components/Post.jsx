bf9cdf75e12c9a28ed29ad7c08d3c98e03a43623
raimundosilvw <mengao-10r@hotmail.com>
2022-12-05 21:38:48 -0300

    Criação do fomulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -25,7 +25,19 @@ export function Post() {
                     <a href=""> #rocketseat</a>
 
                 </p>
-            </div>                                                                                                                                   
+            </div>    
+
+            <form className={styles.comentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                    placeholder='Deixe um comentário'
+                />
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>                                                                                                                                                                            
+            </form>                                                                                                                               
         </article>
     )   
 }
