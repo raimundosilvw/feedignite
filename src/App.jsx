d3e82362955edbdfabd6c128c203416875241573
raimundosilvw <mengao-10r@hotmail.com>
2022-12-13 22:54:34 -0300

    Iterando o JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,53 @@ import styles from './App.module.css';
 
 import './global.css'; 
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/raimundosilvw.png',
+      name: 'Raimundo Silva',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'Raimundo.dev/doctorcare'},
+    ],
+    publishedAt: new Date('2022-12-01 20:00:00'),
+  },
+
+  {
+  id: 2,
+  author: {
+    avatarUrl: 'https://github.com/raimundosilvw.png',
+    name: 'Raimundo',
+    role: 'Web Developer'
+  },
+  content: [
+    { type: 'paragraph', content: 'Fala galeraa 👋'},
+    { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    { type: 'link', content: 'Raimundo.dev/doctorcare'},
+  ],
+  publishedAt: new Date('2022-12-10 20:00:00'),
+},
+
+{
+  id: 3,
+  author: {
+    avatarUrl: 'https://github.com/raimundosilvw.png',
+    name: 'José',
+    role: 'Web Developer'
+  },
+  content: [
+    { type: 'paragraph', content: 'Fala galeraa 👋'},
+    { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    { type: 'link', content: 'Raimundo.dev/doctorcare'},
+  ],
+  publishedAt: new Date('2022-12-15 20:00:00'),
+},
+];
+
 export function App() {
 
   return (
@@ -18,14 +65,15 @@ export function App() {
           <Sidebar />
         </aside>
         <main>
-            <Post 
-                author="Raimndo Silva"
-                content="Lorem ipsun dolor sit consectetur adipiscing elit  "
-            />
-           <Post 
-                author="Raimndo Silva"
-                content="Lorem ipsun dolor sit consectetur adipiscing elit  "
-            />
+            {posts.map(post => {
+              return (
+                <Post 
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            } )}
         </main>
 
       </div>
