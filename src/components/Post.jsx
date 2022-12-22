5b098e49f6710aaed3a8f8d97b1d8a0868470aaf
raimundosilvw <mengao-10r@hotmail.com>
2022-12-21 22:26:19 -0300

    Removendo comentarios (imutabilidade)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,14 @@ export function Post({ author, publishedAt, content}) {
         setNewCommentText(event.target.value.value);
     }
 
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeleteOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        })
+
+        setComments(commentsWithoutDeleteOne)
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -77,7 +85,11 @@ export function Post({ author, publishedAt, content}) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment}/>
+                    return <Comment 
+                                key={comment} 
+                                content={comment} 
+                                onDeleteComment={deleteComment}
+                            />
                 })}
             </div>                                                                                                                        
         </article>
