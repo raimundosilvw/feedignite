71f7232df108c8941d825e783d02e121d02c7cf3
raimundosilvw <mengao-10r@hotmail.com>
2022-12-26 21:24:27 -0300

    Corrigindo erros

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,7 +32,13 @@ export function Post({ author, publishedAt, content}) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
+
         setNewCommentText(event.target.value.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
     }
 
     function deleteComment(commentToDelete) {
@@ -42,6 +48,8 @@ export function Post({ author, publishedAt, content}) {
 
         setComments(commentsWithoutDeleteOne)
     }
+
+    const isNewCommentEmpty = newCommentText.length == 0;
 
     return (
         <article className={styles.post}>
@@ -76,10 +84,14 @@ export function Post({ author, publishedAt, content}) {
                     placeholder='Deixe um comentário'
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid = {handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>                                                                                                                                                                            
             </form>       
 
